c6e0975885b1bb73c1e02b0c0b62c94b62b8d02b
Gerrie Gumban <122905264+gerrieg525@users.noreply.github.com>
2025-02-03 02:21:52 +0000

    Finished Lab 2

---
+++ src/MixMatch.java
@@ -0,0 +1,43 @@
+public class MixMatch {
+    public static void main(String[] args) {
+        //Assignment: Create 8 types of data types
+
+        //Data Type 1: String
+        String var_string = "This is my playlist.";
+        System.out.println(var_string + " <- String, stores a string of characters commonly used to create various texts and sentences.");
+
+        //Data Type 2: Char
+        char var_char = 'a';
+        System.out.println(var_char + " <- Character, stores single letters, numbers, and characters such as $.");
+
+        //Data Type 3: Short
+        short var_short = 11;
+        System.out.println(var_short + " <- Short, stores signed whole integers and primarily used to save memory in larger arrays.");
+       
+        //Data Type 4: Int
+        int var_int = 55;
+        System.out.println(var_int + " <- Integer, stores signed whole integers and is the common data type for storing numbers.");
+
+        //Data Type 5: Long
+        long var_long = 100; 
+        System.out.println(var_long + " <- Long, stores signed whole integers and usually used for integers too large to fit in an integer data type.");
+
+        //Data Type 6: Float
+        //Don't forget to add f to value
+        float var_float = 95.0f;
+        System.out.println(var_float + " <- Float, stores numbers with decimal points and used to store approxiamate numbers.");
+
+        //Data Type 7: Double
+        double var_double = 95.0;
+        System.out.println(var_double + " <- Double, stores numbers with decimal points with high precision.");
+
+        //Data Type 8: Boolean
+        boolean var_bool = true;
+        System.out.println(var_bool + " <- Boolean, stores two values, true(1) or false(0) used to represent conditions and logical operations like AND, OR, NOT.");
+
+        //Practice Outputting: Single Print Method
+        System.out.println("This is a string" + 'p' + 33 + 11 + 100 + 60.0f + 50.0 + true);
+
+    }
+
+}
